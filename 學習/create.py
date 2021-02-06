583f7f757777a281901a1c7555bda1c8e9c5aae3
村名 陳 <creeper531100@gmail.com>
2021-02-06 23:03:06 +0800

    修改了大部分內容，更加高級

--- 學習/create.py
+++ 學習/create.py
@@ -1,19 +1,19 @@
 import os
 import cv2
-def cover():
-    image_folder = 'images'
-    video_name = 'video.mp4'
+import sys
 
+
+def cover(video_name, image_folder):
     vidcap = cv2.VideoCapture(video_name)
     count = 0
     while vidcap.isOpened():
         success, image = vidcap.read()
         if success:
-            cv2.imwrite(os.path.join(image_folder, '%d.png') % count, image)
+            cv2.imwrite(os.path.join(image_folder, '%07d.png') % count, image)
             count += 1
         else:
             break
     cv2.destroyAllWindows()
     vidcap.release()
 
-cover()
+cover(sys.argv[1], sys.argv[2])
--- 學習/main.cpp
+++ 學習/main.cpp
@@ -4,17 +4,21 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "tools.h"
 #include "string.h"
+#include <nlohmann/json.hpp>
+#include <utility>
+#include <fstream>
+#include <iomanip>
+
 
 using namespace cv;
-
 using namespace std;
+using json = nlohmann::json;
 
 string aHash(Mat &&img) {
 	Mat gray;
 	int s = 0;
 	int avg = 0;
-	string hash_str = "";
-	resize(img, img, Size(16, 9), 0, 0, INTER_AREA);
+	string hash_str = "";	
 	cvtColor(img, gray, COLOR_RGB2GRAY);
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 16; j++) {
@@ -30,56 +34,105 @@ string aHash(Mat &&img) {
 				hash_str += '0';
 		}
 	}
+
 	return hash_str;
 }
 
-int cmpHash(string &hash1, string &&hash2) {
-	int n = 0;
-	if (hash1.length() != hash2.length())
-		return -1;
-	for (int i = 0; i < hash1.length(); i++) {
-		n += hash1[i] ^ hash2[i];
+auto cmpHash(string &hash1,const string &&hash2) {
+	json jsonfile;
+	string jsonStr;
+	ifstream in(hash2);
+	in >> jsonfile;
+	int n = 0, j = 0;
+	map<int, int> maps;
+	vector<pair<int, int>> pairs;
+
+	for (string row : jsonfile["HashMap"]) {
+		n = 0;
+		for (int i = 0; i < hash1.length(); i++) {
+			n += hash1[i] ^ row[i];
+			maps[j] = n;
+		}
+		j++;
 	}
-	return n;
-}
+	
+	for (auto &row : maps)
+		pairs.push_back(row);
 
-auto compareImg(string img) {
-	string folder_path = "D:\\source\\學習\\學習\\images\\*.png";
-	vector<string> file_names;
-	float index = 0.0;
-	map<string, int> arraryMap;
-	glob(folder_path, file_names);   //get file names
-	for (auto row : file_names) {
-		arraryMap[row] = cmpHash(img, aHash(imread(row)));
-		printf("%f %%\r", ++index / (float)file_names.size() * 100);
-	}
-
-	vector<pair<string, int>> pairs;
-	for (const auto& row : arraryMap)
-		pairs.emplace_back(row);
-
-	sort(pairs.begin(), pairs.end(), [=](pair<string, int>& a, pair<string, int>& b)
+	sort(pairs.begin(), pairs.end(), [=](pair<int, int>& a, pair<int, int>& b)
 		{
 			return a.second < b.second;
 		}
 	);
-	
-	return pairs;
+
+	return pairs[0].first;
 }
 
+void writeHash(const string &folder_path, string setAnimeName) {
+	json jsonfile;
+	vector<string> file_names;
+	vector<string> indexPath;
+
+	string saveFileName = "json\\";
+	saveFileName = saveFileName + setAnimeName + ".json";
+	float prog = 0.0;
+	glob(folder_path, file_names);
+	cout << endl;
+	for (auto row : file_names) {
+		indexPath.emplace_back(aHash(imread(row)));
+		printf("%s : %f\n", row.c_str(), prog);
+		printf("進度: %f%%\r", ++prog / (float)file_names.size() * 100);
+	}
+	jsonfile["HashMap"] = indexPath;
+	ofstream file(saveFileName);
+	file << jsonfile;
+}
+
+void createImg(string inputDir,  string outputDir) {
+	string commando = "start create.py " + inputDir + " " + outputDir;
+	system(commando.c_str());
+}
 
 int main() {
-	string img0 = aHash(imread("D:\\source\\學習\\學習\\video.png"));
-	auto imagePair = compareImg(img0);
-	string filePath = imagePair[0].first;
-	
-	advstring getPath(filePath);
-	system("cls");
-	string addressRemove = getPath.split("\\")[5];
-	advstring getIndex(addressRemove);
-	string extensionRemove = getIndex.split(".")[0];
-	int calcTime = convert<int>(extensionRemove);
-	
-	system(("start " + filePath).c_str());
-	printf("%02d:%02d",calcTime / 30 / 60, calcTime / 30 % 60);
+	string getInput = input("歡迎使用查圖程式\n(0).拆解圖片 (1).計算相似度 (2).自動: ");
+	string file, imgPath;
+	switch (convert<int>(getInput)) {
+		
+	case 0:
+		file = input("請設定檔案名稱: ");
+		createImg(file, "img");
+		break;
+	case 1:
+		file = input("請設定名稱: ");
+		writeHash("img", file);
+		system("del /F /S img\\*.png");
+		break;
+	case 2:
+		file = input("開啟檔案名稱: ");
+		string compareImg = aHash(imread(file));
+		int getTime = cmpHash(compareImg, "json\\000.json");
+		printf("%02d:%02d", getTime/30/60, getTime/30%60);
+		break;
+	}
 }
+/*void createImg(string inputDir, const string& outputDir) {
+	string fileName;
+	stringstream inter;
+	VideoCapture cap(inputDir);
+	if (!cap.isOpened()){
+		std::cerr << "ERROR: Could not open video " << inputDir << std::endl;
+	}
+	int frame_count = 0;
+	while (cap.isOpened()){
+		Mat frame;
+		cap >> frame; //get a new frame from the video
+		if (frame.empty()){
+			break;
+		}
+		inter << setw(5) << setfill('0') << frame_count << ".jpg";
+		inter >> fileName;
+		string filePath = outputDir + fileName + ".jpg";
+		cv::imwrite(filePath, frame);
+		frame_count++;
+	}
+}*/
